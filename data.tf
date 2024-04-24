cad1925694d9c37bcaf5023e5a5f8d7526060c31
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-24 16:02:45 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,43 +1,59 @@
-data "aws_vpc" "main_vpc"{
+data "aws_vpc" "main_vpc" {
 
   filter {
-    name="tag:Name"
-    values=["main-vpc"]
+    name   = "tag:Name"
+    values = ["main-vpc"]
   }
 }
 
-data "aws_subnets" "private"{
+data "aws_subnets" "private" {
   filter {
-    name="vpc-id"
-    values=[data.aws_vpc.main_vpc.id]
+    name   = "vpc-id"
+    values = [data.aws_vpc.main_vpc.id]
   }
   filter {
-    name="tag:Subnet-Type"
-    values=["private"]
+    name   = "tag:Subnet-Type"
+    values = ["private"]
   }
 }
 
-data "aws_subnet" "private_subnets"{
-  for_each=toset(data.aws_subnets.private.ids)
-  id = each.value
+data "aws_subnet" "private_subnets" {
+  for_each = toset(data.aws_subnets.private.ids)
+  id       = each.value
 }
 
-data "aws_security_group" "EKS-Security-Group"{
+data "aws_security_group" "EKS-Security-Group" {
   name = "eks-cluster-sg"
 }
 
-data "aws_subnets" "public"{
+data "aws_subnets" "public" {
   filter {
-    name="vpc-id"
-    values=[data.aws_vpc.main_vpc.id]
+    name   = "vpc-id"
+    values = [data.aws_vpc.main_vpc.id]
   }
   filter {
-    name="tag:Subnet-Type"
-    values=["public"]
+    name   = "tag:Subnet-Type"
+    values = ["public"]
   }
 }
 
-data "aws_subnet" "public_subnets"{
-  for_each=toset(data.aws_subnets.public.ids)
-  id = each.value
+data "aws_subnet" "public_subnet_1" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main_vpc.id]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["public_subnet_1"]
+  }
 }
+
+data "aws_subnet" "public_subnets" {
+  for_each = toset(data.aws_subnets.public.ids)
+  id       = each.value
+}
+/*
+data "aws_kms_key" "my_key" {
+  key_id = "arn:aws:kms:region:account-id:key/key-id" #enter your existing kms key id
+}
+*/
