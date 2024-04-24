539578f7d1c1ab7ce9dcc26b9939225531dff3ac
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-24 16:25:49 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -17,6 +17,24 @@ resource "aws_instance" "my_ec2" {
     destination = "/home/ec2-user/.ssh/jenkins-key.pem"
   }
 */
+root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 30
+    encrypted             = true
+    kms_key_id            = data.aws_kms_key.my_key.arn  # Specify your KMS key ID
+    delete_on_termination = true
+  }
+
+  # Additional EBS volume
+  ebs_block_device {
+    device_name           = "/dev/sdf"
+    volume_type           = "gp2"
+    volume_size           = 100
+    encrypted             = true
+    kms_key_id            = data.aws_kms_key.my_key.arn  # Specify your KMS key ID
+    delete_on_termination = true
+  }
+
   user_data = <<EOF
 #!/bin/bash
 set -x
@@ -24,6 +42,9 @@ sudo yum update -y
 sudo yum install java-11-openjdk java-11-openjdk-devel -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+sudo mkfs.ext4 /dev/xvdf
+sudo mkdir /apps
+sudo mount /dev/xvdf /apps
 sudo yum install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
