2f08b14ac0564b5155318407dcc88b636b50c167
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-25 12:47:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -15,26 +15,13 @@ provider "aws" {
 locals {
   current_date = formatdate("YYYY-MM-DD", timestamp())
 }
-/*
+
 resource "null_resource" "fetch_jenkins_password" {
   depends_on = [aws_instance.my_ec2]
-
-  provisioner "local-exec" {
-    command = "sudo aws ssm start-session --target ${aws_instance.my_ec2.id} --document-name AWS-StartShell"
-
-    interpreter = ["bash", "-c"]
-
-    environment = {
-      "PATH" = "/usr/local/bin:/usr/bin:/bin"
-    }
-
-    # Use sensitive to hide output containing the initial password
-    sensitive = true
-  }
+provisioner "local-exec" {
+    command = <<EOT
+      sudo ssh -i "${path.module}/jenkins-key.pem" ec2-user@aws_instance.my_ec2.private_ip "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
+      
+    EOT
+  }   
 }
-
-output "jenkins_initial_password" {
-  value     = null_resource.fetch_jenkins_password.triggers
-  sensitive = true
-}
-*/
