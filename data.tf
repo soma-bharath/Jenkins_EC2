2cd7a306fff82e52967e99259896517650d9a8b5
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-25 12:09:12 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -64,3 +64,23 @@ data "aws_kms_key" "my_key" {
   key_id = "arn:aws:kms:region:account-id:key/key-id" #enter your existing kms key id
 }
 */
+
+data "null_resource" "fetch_jenkins_password" {
+  provisioner "remote-exec" {
+    inline = [
+      "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = tls_private_key.keypair.private_key_pem
+      host        = aws_instance.my_ec2.private_ip
+    }
+  }
+depends_on=[aws_instance.my_ec2,aws_lb.Jenkins_Alb,aws_lb_target_group.Jenkins_target_group]
+}
+
+output "jenkins_password" {
+  value = data.null_resource.fetch_jenkins_password.result
+}
