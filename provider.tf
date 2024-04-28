379c71bddb40ed2694fe4ed279222e911b6a3187
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-28 21:07:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -22,7 +22,8 @@ provisioner "local-exec" {
     command = <<EOT
       sudo chmod 700 jenkins-key.pem && \
       sleep 20 && \
-      sudo ssh -o StrictHostKeyChecking=no -i "${path.module}/jenkins-key.pem" ec2-user@${aws_instance.my_ec2.private_ip} "sudo cat /apps/secrets/initialAdminPassword"
+      sudo ssh -o StrictHostKeyChecking=no -i "${path.module}/jenkins-key.pem" ec2-user@${aws_instance.my_ec2.private_ip} "sudo cat /apps/secrets/initialAdminPassword; sleep 30; sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent; sleep 30; sudo systemctl restart firewalld"
+
       
     EOT
   }   
