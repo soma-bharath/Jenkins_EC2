bbd12b495f18c7a37f89a228e79c148bcf247e7a
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-04 14:47:30 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_ec2" {
   instance_type               = "t2.large"
-  ami                         = "ami-04fd4a41214d8887d" #CIS AMI ID in us-west-2 region
+  ami                         = "ami-050d5e0efb5579230" #CIS AMI ID in us-west-2 region
   subnet_id                   = data.aws_subnet.private_subnet_1.id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   key_name                    = aws_key_pair.jenkins_key_pair.key_name
@@ -20,7 +20,7 @@ resource "aws_instance" "my_ec2" {
 */
 root_block_device {
     volume_type           = "gp2"
-    volume_size           = 30
+    volume_size           = 500
     encrypted             = true
     #kms_key_id            = data.aws_kms_key.my_key.arn  # Specify your KMS key ID
     delete_on_termination = true
@@ -30,7 +30,7 @@ root_block_device {
   ebs_block_device {
     device_name           = "/dev/sdf"
     volume_type           = "gp2"
-    volume_size           = 100
+    volume_size           = 1000
     encrypted             = true
     #kms_key_id            = data.aws_kms_key.my_key.arn  # Specify your KMS key ID
     delete_on_termination = true
@@ -39,20 +39,20 @@ root_block_device {
   user_data = <<EOF
 #!/bin/bash
 set -xe
-sudo mkfs -t ext4 /dev/xvdf
+sudo mkfs -t ext4 /dev/nvme1n1
 sudo mkdir /apps
-sudo mount /dev/xvdf /apps
-sudo echo "/dev/xvdf /apps ext4 defaults,nofail 0 2" >> sudo /etc/fstab
+sudo mount /dev/nvme1n1 /apps
+sudo echo "/dev/nvme1n1 /apps ext4 defaults,nofail 0 2" >> sudo /etc/fstab
 sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://downloads.cloudbees.com/cje/rolling/rpm/jenkins.repo
 sudo rpm --import https://downloads.cloudbees.com/jenkins-enterprise/rolling/rpm/cloudbees.com.key
 sudo yum upgrade -y
-sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm --nogpgcheck
 sudo systemctl start amazon-ssm-agent
 sudo systemctl enable amazon-ssm-agent
-sudo yum install java-11-openjdk-devel -y
+sudo yum install java-11-amazon-corretto-devel -y
 sudo wget https://jenkins-downloads.cloudbees.com/cje/rolling/rpm/RPMS/noarch/jenkins-2.346.4.1-1.1.noarch.rpm
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y --skip-broken
 sudo yum install jenkins-2.346.4.1-1.1.noarch.rpm -y
 sudo systemctl start jenkins
 sudo chmod 777 -R /apps/
@@ -69,6 +69,10 @@ sleep 20
 sudo systemctl start firewalld
 sleep 40
 sudo systemctl enable firewalld
+sleep 30
+sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
+sleep 30
+sudo systemctl restart firewalld
 password=$(sudo cat /apps/secrets/initialAdminPassword)
 echo "$password"
 EOF
