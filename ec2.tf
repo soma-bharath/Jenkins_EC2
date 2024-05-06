5d602e897bd7a1465ea71e1fdd0c72a81ce9ce74
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-06 11:05:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -36,26 +36,36 @@ root_block_device {
     delete_on_termination = true
   }
 
-  user_data = <<EOF
-#!/bin/bash
+  user_data = <<EOF#!/bin/bash
 set -xe
-sudo mkfs -t ext4 /dev/nvme1n1
+sudo mkfs -t ext4 /dev/xvdf
 sudo mkdir /apps
-sudo mount /dev/nvme1n1 /apps
-sudo echo "/dev/nvme1n1 /apps ext4 defaults,nofail 0 2" >> sudo /etc/fstab
+sudo mount /dev/xvdf /apps
+sudo echo "/dev/xvdf /apps ext4 defaults,nofail 0 2" >> sudo /etc/fstab
 sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://downloads.cloudbees.com/cje/rolling/rpm/jenkins.repo
 sudo rpm --import https://downloads.cloudbees.com/jenkins-enterprise/rolling/rpm/cloudbees.com.key
 sudo yum upgrade -y
-sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm --nogpgcheck
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 sudo systemctl start amazon-ssm-agent
 sudo systemctl enable amazon-ssm-agent
-sudo yum install java-11-amazon-corretto-devel -y
+sudo cd /opt
+sudo wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
+sudo tar xvf jdk-17_linux-x64_bin.tar.gz
+sudo chown -R root: jdk-17.0.11
+sudo update-alternatives --install /usr/bin/java java /opt/jdk-17.0.11/bin/java 1
+sudo update-alternatives --install /usr/bin/jar jar /opt/jdk-17.0.11/bin/jar 1
+sudo cd
+sudo yum install daemonize -y
 sudo wget https://jenkins-downloads.cloudbees.com/cje/rolling/rpm/RPMS/noarch/jenkins-2.346.4.1-1.1.noarch.rpm
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y --skip-broken
-sudo yum install jenkins-2.346.4.1-1.1.noarch.rpm -y
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+sudo chmod 777 jenkins-2.346.4.1-1.1.noarch.rpm
+sudo rpm -ivh --nodigest --nofiledigest jenkins-2.346.4.1-1.1.noarch.rpm
 sudo systemctl start jenkins
 sudo chmod 777 -R /apps/
+sudo sed -i 's/User=jenkins/User=root/g' /usr/lib/systemd/system/jenkins.service
+sudo systemctl daemon-reload
+sudo systemctl restart jenkins
 sudo systemctl stop jenkins
 sleep 5
 sudo sed -i 's#^JENKINS_HOME=.*#JENKINS_HOME="/apps/"#' /etc/sysconfig/jenkins
@@ -69,10 +79,6 @@ sleep 20
 sudo systemctl start firewalld
 sleep 40
 sudo systemctl enable firewalld
-sleep 30
-sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
-sleep 30
-sudo systemctl restart firewalld
 password=$(sudo cat /apps/secrets/initialAdminPassword)
 echo "$password"
 EOF
