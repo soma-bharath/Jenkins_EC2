69b51904f29885625733e546fee776710ed2a18f
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-14 22:18:44 +0530

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,17 +1,12 @@
-resource "aws_route53_zone" "jenkins_zone" {
-  name = "dev.nn" # Update with your desired domain name
-depends_on=[aws_lb.Jenkins_Alb]
-}
-
 resource "aws_route53_record" "jenkins_record" {
-  zone_id = aws_route53_zone.jenkins_zone.zone_id
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
   name    = "dev.nn"  # Update with your desired domain name
   type    = "A"
   ttl     = "300"
 
   alias {
     name                   = aws_lb.Jenkins_Alb.dns_name
-    zone_id                = aws_route53_zone.jenkins_zone.zone_id
+    zone_id                = data.aws_route53_zone.hosted_zone.zone_id
     evaluate_target_health = false
   }
 depends_on=[aws_route53_zone.jenkins_zone]
