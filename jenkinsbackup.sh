dddf685642a7365930fd2ef94c8209362a18c612
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-23 10:53:30 +0530

    Update jenkinsbackup.sh

--- jenkinsbackup.sh
+++ jenkinsbackup.sh
@@ -5,11 +5,11 @@ BACKUP_PATH="/apps"
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 BACKUP_FILE="jenkins-backup-$TIMESTAMP.tar.gz"
 
-# Create a tar.gz of the Jenkins data
-tar -czvf /tmp/$BACKUP_FILE -C $BACKUP_PATH .
+echo "Creating a tar.gz of the Jenkins data"
+sudo tar -czvf /tmp/$BACKUP_FILE -C $BACKUP_PATH .
 
-# Copy the backup file to S3
-aws s3 cp /tmp/$BACKUP_FILE s3://$S3_BUCKET/
+echo "Copying the backup file to S3"
+sudo aws s3 cp /tmp/$BACKUP_FILE s3://$S3_BUCKET/
 
-# Remove the local backup file
-rm /tmp/$BACKUP_FILE
+echo "Removing the local backup file"
+sudo rm /tmp/$BACKUP_FILE
