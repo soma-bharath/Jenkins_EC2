eed873c630bd6bd3f15c5349b80ec04dd08afd28
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-23 10:56:01 +0530

    Update iam_roles.tf

--- iam_roles.tf
+++ iam_roles.tf
@@ -14,3 +14,59 @@ resource "aws_iam_instance_profile" "EC2_Jenkins" {
   name = "AmazonEC2Jenkins"
   role = aws_iam_role.Amazon_EC2_Jenkins.id
 }
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_s3_access_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "ec2_s3_access_policy"
+  description = "Policy to allow EC2 instance to access S3 bucket for backups"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "arn:aws:s3:::my-jenkins-backup-bucket/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = "arn:aws:s3:::my-jenkins-backup-bucket"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_s3_access_instance_profile"
+  role = aws_iam_role.ec2_role.name
+}
