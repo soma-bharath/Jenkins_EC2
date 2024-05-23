440984f7fa82d796ee10b0388251e54acf470007
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-23 11:01:10 +0530

    Update iam_roles.tf

--- iam_roles.tf
+++ iam_roles.tf
@@ -2,36 +2,19 @@ resource "aws_iam_role" "Amazon_EC2_Jenkins" {
   assume_role_policy    = "{\"Statement\":[{\"Action\":\"sts:AssumeRole\",\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"ec2.amazonaws.com\"}}],\"Version\":\"2012-10-17\"}"
   description           = "Allows EC2 instances to call AWS services on your behalf."
   force_detach_policies = false
-  managed_policy_arns   = ["arn:aws:iam::aws:policy/AmazonSSMManagedEC2InstanceDefaultPolicy", "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
+  managed_policy_arns   = ["arn:aws:iam::aws:policy/AmazonSSMManagedEC2InstanceDefaultPolicy", "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",aws_iam_policy.s3_access_policy.arn]
   max_session_duration  = 3600
   name                  = "AmazonEC2Jenkins"
   name_prefix           = null
   path                  = "/"
   permissions_boundary  = null
+  depends_on            = [aws_iam_policy.s3_access_policy]
 }
 
 resource "aws_iam_instance_profile" "EC2_Jenkins" {
   name = "AmazonEC2Jenkins"
   role = aws_iam_role.Amazon_EC2_Jenkins.id
 }
-
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2_s3_access_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
 
 resource "aws_iam_policy" "s3_access_policy" {
   name        = "ec2_s3_access_policy"
@@ -59,14 +42,4 @@ resource "aws_iam_policy" "s3_access_policy" {
       }
     ]
   })
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_policy_attachment" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.s3_access_policy.arn
-}
-
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "ec2_s3_access_instance_profile"
-  role = aws_iam_role.ec2_role.name
 }
