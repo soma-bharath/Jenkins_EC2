d2152e403980c06e4b22b08834bd3dabac35e63e
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-23 11:11:50 +0530

    Create vpc-s3-endpoint.tf

---
+++ vpc-s3-endpoint.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = data.aws_vpc.main_vpc.id
+  service_name = "com.amazonaws.us-west-1.s3"
+  route_table_ids = [data.aws_route_tables.private.id] 
+
+  tags = {
+    Name = "S3VPC"
+    Date = local.current_date
+    Env  = var.env
+  }
+}
