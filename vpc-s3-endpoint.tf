7cacc32d08da51a446a6f4ec4dc372511f5d75ec
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-27 13:34:02 +0530

    Update vpc-s3-endpoint.tf

--- vpc-s3-endpoint.tf
+++ vpc-s3-endpoint.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = data.aws_vpc.main_vpc.id
-  service_name = "com.amazonaws.us-west-1.s3"
+  service_name = "com.amazonaws.us-gov-west-1.s3"
   route_table_ids = [data.aws_route_tables.private.id] 
 
   tags = {
